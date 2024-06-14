100de3d5ba0db852a2dfeb68c4e6c8f52a252cb4
Nicolas Goudry <goudry.nicolas@gmail.com>
2024-06-15 00:33:53 +0200

    fix(nix): package name and homepage

--- package.nix
+++ package.nix
@@ -1,14 +1,14 @@
 { lua, lib, ... }:
 
 lua.pkgs.buildLuaPackage {
-  pname = "rndname";
+  pname = "rndname-lua";
   version = "1.0.0";
   src = ./.;
   dontBuild = true;
 
   meta = {
     description = "Lua library to generate random names";
-    homepage = "https://github.com/nicolas-goudry/rndname";
+    homepage = "https://github.com/nicolas-goudry/rndname.lua";
     license = lib.licenses.mit;
     maintainers = [ lib.maintainers.nicolas-goudry ];
   };
